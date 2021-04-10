ae42486c1a02831e131d9401b77f8109f2d623e3
Felipe Marcondes <felipemgcarvalho@gmail.com>
2021-04-10 14:02:43 -0300

    Primeiro Retorno da API ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React , {useEffect, useState} from 'react';
+import Countries from './components/Countries';  
 import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState([])
+
+
+
+  useEffect(()=>{
+    getCountries()
+  }, []) // com os brackets vazios [] como segundo argumento o useEffect roda apenas uma vez na montagem da aplicação, colocando as consts ele roda quando forem chamadas
+
+  const getCountries = async () => {  //forma alternativa de chamar fetch request
+    const response = await fetch('https://restcountries.eu/rest/v2/all')
+    const data = await response.json()
+    setCountries(data)
+    console.log(data)    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Pesquisar</button>        
+      </form>
+
+      {countries.map(countries => (
+        <Countries 
+        key={countries.name}      
+        flag={countries.flag} 
+        name={countries.name} 
+        population={countries.population} 
+        region={countries.region} 
+        capital={countries.capital} />
+      ))}
+
     </div>
   );
 }
---
+++ src/components/Countries.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Countries = ({flag, name,population,region,capital}) => {
+    return (
+        <div>
+            <img src={flag} alt="" />
+            <h1>{name}</h1>
+            <p>População: {population}</p>
+            <p>Continente: {region}</p>
+            <p>Capital: {capital}</p>
+
+
+        </div>
+    )
+}
+
+export default Countries
