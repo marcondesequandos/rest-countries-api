45abfe825249172c0a47aafb38712bfa3d48d922
Felipe Marcondes <felipemgcarvalho@gmail.com>
2021-04-11 10:51:52 -0300

    Filtro por Região Implementado

--- src/App.js
+++ src/App.js
@@ -5,25 +5,77 @@ import './App.css';
 function App() {
 
   const [countries, setCountries] = useState([])
+  const [region, setRegion] = useState('')
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('')
 
 
 
   useEffect(()=>{
     getCountries()
-  }, []) // com os brackets vazios [] como segundo argumento o useEffect roda apenas uma vez na montagem da aplicação, colocando as consts ele roda quando forem chamadas
+    getCountriesByName()
+    getCountriesByRegion()
+  }, [query, region]) // com os brackets vazios [] como segundo argumento o useEffect roda apenas uma vez na montagem da aplicação, colocando as consts ele roda quando forem chamadas
 
   const getCountries = async () => {  //forma alternativa de chamar fetch request
     const response = await fetch('https://restcountries.eu/rest/v2/all')
     const data = await response.json()
     setCountries(data)
-    console.log(data)    
+    console.log(data)
+    console.log(data.region)     
   }
+
+  const getCountriesByName = async () => {  //forma alternativa de chamar fetch request
+    const response = await fetch(`https://restcountries.eu/rest/v2/name/${query}`)
+    const data = await response.json()
+    setCountries(data)
+    console.log(data)
+       
+  } //11/04 Está dando um erro que não atrapalha na visualização, mas é um erro. Quando a pesquisa está vazia ao abrir a aplicação
+
+  const getCountriesByRegion = async () => {  //forma alternativa de chamar fetch request
+    const response = await fetch(`https://restcountries.eu/rest/v2/region/${region}`)
+    const data = await response.json()
+    setCountries(data)
+    console.log(data)
+    console.log(data.region)    
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
+
+  const updateRegion = e => {
+    setRegion(e.target.value)
+  }
+
+  const getRegion = e => {
+    setRegion(region)
+  }
+
+
 
   return (
     <div className="App">
-      <form>
-        <input className="search-bar" type="text"/>
-        <button className="search-button" type="submit">Pesquisar</button>        
+      <form onSubmit={getSearch , getRegion}>
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+        <button className="search-button" type="submit">Pesquisar</button>
+        <label>Filter by Region:</label>
+        <select type="submit" onChange={updateRegion} name="Filter by Region">
+          <option selected></option>
+          <option>Africa</option>
+          <option>Americas</option>
+          <option>Asia</option>
+          <option>Europe</option>
+          <option>Oceania</option>
+          <option>Polar</option> 
+        </select>        
       </form>
 
       {countries.map(countries => (
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 const Countries = ({flag, name,population,region,capital}) => {
     return (
-        <div>
+        <div title="get details">
             <img src={flag} alt="" />
             <h1>{name}</h1>
             <p>População: {population}</p>
             <p>Continente: {region}</p>
             <p>Capital: {capital}</p>
-
-
         </div>
     )
 }
