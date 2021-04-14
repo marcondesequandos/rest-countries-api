90823fb16aa76b06b4fba6e23dc6b15f933a827b
Felipe Marcondes <felipemgcarvalho@gmail.com>
2021-04-14 11:07:08 -0300

    Escopo 1 (Parte Funcional) Finalizado

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ function App() {
 
   useEffect(()=>{
     getCountries()
-    getCountriesByName(query)
-    getCountriesByRegion(region)
+    getCountriesByName()
+    getCountriesByRegion() 
+
   }, [query, region]) // com os brackets vazios [] como segundo argumento o useEffect roda apenas uma vez na montagem da aplicação, colocando as consts ele roda quando forem chamadas
 
   const getCountries = async () => {  //forma alternativa de chamar fetch request
@@ -29,30 +30,40 @@ function App() {
   }
 
   const getCountriesByName = async () => {  //forma alternativa de chamar fetch request
+
+    if (query === '')
+    {
+      return null
+    } else {
     
     const response = await fetch(`https://restcountries.eu/rest/v2/name/${query}`)
     const data = await response.json()
     setCountries(data)
-    // console.log(data[query].languages)
+
+
+    }
    
    
 
   
        
-  } //11/04 Está dando um erro que não atrapalha na visualização, mas é um erro. Quando a pesquisa está vazia ao abrir a aplicação
-  //Tenho uma solução proposta para acabar com esse erro: https://stackoverflow.com/questions/59804016/how-to-fetch-api-data-asynchronously-with-query-from-input-field
-  //fazer uma const pegando form event.target if parameter === ""... verificar como ficariaisso com as consts get Region e search que fiz..
-  //fazer via refs de elemento dom https://pt-br.reactjs.org/docs/refs-and-the-dom.html
-  // fazer um retorno para valor invalido
-  // As 3 funções estão dando setCountries verificar se não é o problema aqui olhar no meu código da timezoneapi
-  //Uma ideia: Fazer um componente pra cada Fetch pois ai vai renderizar separado setCountries SetCountry SetCountryByRegion
+  } 
+
+
 
 
   const getCountriesByRegion = async () => {  //forma alternativa de chamar fetch request
-    const response = await fetch(`https://restcountries.eu/rest/v2/region/${region}`)
-    const data = await response.json()
-    setCountries(data)
-    
+
+    if (region === '')
+    {
+      return null
+    } else {
+
+      const response = await fetch(`https://restcountries.eu/rest/v2/region/${region}`)
+      const data = await response.json()
+      setCountries(data)
+
+    }   
         
   }
 
@@ -60,20 +71,32 @@ function App() {
     setSearch(e.target.value)
   }
 
-  const getSearch = e => {
+  const getSearch = e => {    
     e.preventDefault()
     setQuery(search)
+    console.log(search)   
     setSearch('')
+    
   }
 
   const updateRegion = e => {
     setRegion(e.target.value)
+
+   
+    
   }
 
+
+
   const getRegion = e => {
-    e.preventDefault()
+    preventDefault(e)    
     setRegion(region)
+    console.log(region)
+    
+    
+    
   }
+
 
 
 
@@ -83,9 +106,10 @@ function App() {
         <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">Pesquisar</button>
       </form>
-      <form onSubmit={getRegion}>
+      <form id={"Form"} onSubmit={getRegion}>
         <label>Filter by Region:</label>
-        <select type="submit" value="" onChange={updateRegion} name="Filter by Region">
+        <input  className="search-bar" type="text" value={region} onChange={updateSearch}/>
+        <select value={region}  name="Filter by Region" onChange={updateRegion}>
           <option></option>
           <option>Africa</option>
           <option>Americas</option>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import CountryDetail from './components/CountryDetail/CountryDetail'
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
   <React.StrictMode>   
       <Router>
@@ -21,3 +22,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
